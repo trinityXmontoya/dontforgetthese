19d52df32eb9195f799adf2b01f5895c9dad03a6
trinityXmontoya <trinityXmontoya@gmail.com>
2014-07-18 14:37:53 -0400

    basic ui changes

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   loadBackboneVars();
   loadKeywordList();
   shareList();
+  modal();
 });
 
 function loadBackboneVars(){
@@ -20,6 +21,23 @@ function loadKeywordList(){
 
 function shareList(){
   $('.share-list').on('click', function(){
+    twitterButton();
+    fbookButton();
+    $('#modal').toggleClass('display-me');
     $('#share-options').toggleClass('display-me')
   })
 }
+
+var twitterButton = function(){
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+}
+
+var fbookButton = function(){
+   (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&appId=274400686074344&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
+}
+
